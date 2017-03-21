b33ee3b5d5a9cb70fae453bd8824581cb89a251c
kristy <wangyingfinance@outlook.com>
2017-03-21 22:26:50 +0800

    3

--- DBconnect.py
+++ DBconnect.py
@@ -7,6 +7,7 @@ Created on Tue Mar 07 15:33:29 2017
 
 import cx_Oracle as ora
 import numpy
+import pandas
 
 dbcenter_dsn = ora.makedsn(host= "172.16.8.20", port="1521", sid= "dbcenter")
 dbcenter_conn_Ora = ora.connect('READER', 'reader', dbcenter_dsn)
